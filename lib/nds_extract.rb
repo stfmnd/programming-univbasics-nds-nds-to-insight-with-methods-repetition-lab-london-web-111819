601ed1eec6e8a71ae5f9b017b133e8b864897b37
Matthew <matthew.97.clark@gmail.com>
2019-10-30 18:32:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,7 +45,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  directors_totals
+  directors_totals(source)
 end
 
 
